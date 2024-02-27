554cecd16edad15a05333d847c0351869b42876a
gregoryptheriot <griot86@gmail.com>
2024-02-27 12:01:32 -0600

    Initial checkin
    let's do it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { IssueComponent } from './components/issue/issue.component';
+import { ResourcesComponent } from './components/resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'issues/:id', component: IssueComponent },
+  { path: 'issues/:id/resources', component: ResourcesComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IssueComponent } from './components/issue/issue.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueComponent,
+    ResourcesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
